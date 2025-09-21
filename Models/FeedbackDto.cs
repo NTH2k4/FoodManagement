d00967e1cfe95b0ff2066963e2ff98c41c4e9594
Beater <trunghieu14092004@gmail.com>
2025-09-21 14:18:47 +0700

    update feedbackDto

---
+++ Models/FeedbackDto.cs
@@ -0,0 +1,13 @@
+﻿namespace FoodManagement.Models
+{
+    public class FeedbackDto
+    {
+        public string id { get; set; } = default!;
+        public string accountId { get; set; } = default!;
+        public string? name { get; set; }
+        public string? phone { get; set; }
+        public string? email { get; set; }
+        public string? comment { get; set; }
+        public long createdAt { get; set; }
+    }
+}
