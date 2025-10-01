39ef22c6e08f789fc6dd9c6c49c74b9a3fae214d
Beater <trunghieu14092004@gmail.com>
2025-10-01 14:33:18 +0700

    add Info and support page

---
+++ Pages/Other/InfomationPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FoodManagement.Pages.Other
+{
+    public class InfomationPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
